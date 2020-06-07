9a7bbdb7dc6a2c8f553dc383ef4e35bab00435a5
Lyle Baer <reabelyl@live.com>
2020-06-06 18:08:07 -0600

    Add Day 7 Work

---
+++ day_7/10_little_monkeys.rb
@@ -0,0 +1,38 @@
+puts "How many monkeys are jumping on the bed to start?"
+
+y = gets.chomp.to_i
+x = 0
+
+y.times do
+  x += 1
+  if x == 10
+        p "Ten little monkey jumping on the bed, He fell off and bumped his head, Mama called the doctor and the doctor said, Get those monkeys right to bed!"
+      end
+  if x == 9
+        p "Nine little monkey jumping on the bed, He fell off and bumped his head, Mama called the doctor and the doctor said, Get those monkeys right to bed!"
+      end
+  if x == 8
+        p "Eight little monkey jumping on the bed, He fell off and bumped his head, Mama called the doctor and the doctor said, Get those monkeys right to bed!"
+      end
+  if x == 7
+        p "Seven little monkey jumping on the bed, He fell off and bumped his head, Mama called the doctor and the doctor said, Get those monkeys right to bed!"
+      end
+  if x == 6
+        p "Six little monkey jumping on the bed, He fell off and bumped his head, Mama called the doctor and the doctor said, Get those monkeys right to bed!"
+      end
+  if x == 5
+        p "Five little monkey jumping on the bed, He fell off and bumped his head, Mama called the doctor and the doctor said, Get those monkeys right to bed!"
+      end
+  if x == 4
+        p "Four little monkey jumping on the bed, He fell off and bumped his head, Mama called the doctor and the doctor said, Get those monkeys right to bed!"
+      end
+  if x == 3
+        p "Three little monkey jumping on the bed, He fell off and bumped his head, Mama called the doctor and the doctor said, Get those monkeys right to bed!"
+      end
+  if x == 2
+        p "Two little monkey jumping on the bed, He fell off and bumped his head, Mama called the doctor and the doctor said, Get those monkeys right to bed!"
+      end
+  if x == 1
+        p "One little monkey jumping on the bed, He fell off and bumped his head, Mama called the doctor and the doctor said, Get those monkeys right to bed!"
+  end
+end
