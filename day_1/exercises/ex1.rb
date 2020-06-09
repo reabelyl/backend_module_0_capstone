de89f76c468bc20be147863975de2e2cfb0e54d1
Lyle Baer <reabelyl@live.com>
2020-06-09 16:23:48 -0600

    Explained Comments & One Line Runs in ex1.rb

--- day_1/exercises/ex1.rb
+++ day_1/exercises/ex1.rb
@@ -1,8 +1,9 @@
-puts "Hello World!"
-puts "Hello Again"
-puts "I like typing this."
-puts "This is fun."
-puts "Yay! Printing."
-puts "I'd much rather you 'not'."
-puts 'I "said" do not touch this.'
+# puts "Hello World!"
+# puts "Hello Again"
+# puts "I like typing this."
+# puts "This is fun."
+# puts "Yay! Printing."
+# puts "I'd much rather you 'not'."
+# puts 'I "said" do not touch this.'
+# Is used for comments and hides the line of code so it will not run...
 p "What is your favorite cat breed?"
