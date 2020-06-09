181927d5f444d385fc3f55636a4e70486438cb96
Lyle Baer <reabelyl@live.com>
2020-06-09 17:36:20 -0600

    Added Variables to Convert incches to cm & lbs to kilos Based on user choice to convert

--- day_1/exercises/ex5.rb
+++ day_1/exercises/ex5.rb
@@ -1,17 +1,36 @@
-my_name = 'Zed A. Shaw'
-my_age = 35 # not a lie in 2009
-my_height = 74 # inches
-my_weight = 180 # lbs
-my_eyes = 'Blue'
-my_teeth = 'White'
-my_hair = 'Brown'
+name = 'Zed A. Shaw'
+age = 35 # not a lie in 2009
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
 
-puts "Let's talk about #{my_name}."
-puts "He's #{my_height} inches tall."
-puts "He's #{my_weight} pounds heavy."
+# 1 inch is 2.54 centimeters
+cm = 2.54
+# 1 pound is 0.45 kilograms
+kilo = 0.45
+
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall."
+puts "He's #{weight} pounds heavy."
 puts "Actually that's not too heavy."
-puts "He's got #{my_eyes} eyes and #{my_hair} hair."
-puts "His teeth are usually #{my_teeth} depending on the coffee."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+
+# Question for user to convert measurements to centimeters and kilograms
+puts "Would you like to see your measurements converted to centimeters and kilograms? (Type 1 for yes and 2 for no)"
+
+x = gets.chomp.to_i
+
+if x == 1
+  puts "Your height in centimeters is: #{height * cm}"
+  puts "Your height in centimeters is: #{weight * kilo}"
+else
+  puts "Ok, not converting... Your Height is: #{height} inches"
+  puts "Ok, not converting... Your Weight is: #{weight} pounds"
+end
 
 # this line is tricky, try to get it exactly right
-puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
