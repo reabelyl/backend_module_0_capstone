f59024ce29915f62b362bee2d52a8e87e77e1765
Lyle Baer <reabelyl@live.com>
2020-06-09 17:12:52 -0600

    Comments to Explain in ex3.rb

--- day_1/exercises/ex3.rb
+++ day_1/exercises/ex3.rb
@@ -1,23 +1,28 @@
+# Prints "I will now count my chickens:" to terminal
 puts "I will now count my chickens:"
-
+# Prints "Hens 25 + 30 / 6" aka "30" to terminal
 puts "Hens #{25 + 30 / 6}"
+# Prints "Roosters 100 - 25 * 3 % 4" aka "97" to terminal
 puts "Roosters #{100 - 25 * 3 % 4}"
-
+# Prints to terminal: "Now I will count the eggs:"
 puts "Now I will count the eggs:"
-
+# Prints to terminal: "6.75"
 puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
-
+# Prints to terminal: "Is it true that 3 + 2 < 5 - 7?"
 puts "Is it true that 3 + 2 < 5 - 7?"
-
+# Prints to terminal: "false"
 puts 3 + 2 < 5 - 7
-
+# Prints to terminal: "What is 3 + 2? 5"
 puts "What is 3 + 2? #{3 + 2}"
+# Prints to terminal: "What is 5 - 7? -2"
 puts "What is 5 - 7? #{5 - 7}"
-
+# Prints to terminal: "Oh, that's why it's false."
 puts "Oh, that's why it's false."
-
+# Prints to terminal: "How about some more."
 puts "How about some more."
-
+# Prints to terminal: "Is it greater? true"
 puts "Is it greater? #{5 > -2}"
+# Prints to terminal: "Is it greater or equal? true"
 puts "Is it greater or equal? #{5 >= -2}"
+# Prints to terminal: "Is it less or equal? false"
 puts "Is it less or equal? #{5 <= -2}"
