98b984c735bad768bc456e7acd8c959cbdddc335
Lyle Baer <reabelyl@live.com>
2020-06-09 18:12:42 -0600

    Ex4 Updated

--- day_1/exercises/ex4.rb
+++ day_1/exercises/ex4.rb
@@ -1,10 +1,10 @@
 cars = 100
-space_in_a_car = 4.0
+space_in_a_car = 4
 drivers = 30
 passengers = 90
 cars_not_driven = cars - drivers
 cars_driven = drivers
-carpool_capacity = cars_driven * space_in_a_car
+carpool_capacity = cars_driven * space_in_a_car # The first time the code was created this variable was not defined
 average_passengers_per_car = passengers / cars_driven
 
 
